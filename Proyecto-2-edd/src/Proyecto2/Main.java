bc58f3095852224464cfeb1c63402bea0018ba76
royvicente90 <r.roy@correo.unimete.edu.ve>
2020-12-04 11:54:01 -0400

    Primer Commit

--- Proyecto-2-edd/src/Proyecto2/Main.java
+++ Proyecto-2-edd/src/Proyecto2/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String [] args) {
         
         
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundoi");
         
     }
     
